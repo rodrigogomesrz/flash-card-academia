67a315caf79e8c75c82e1cc25672068c907a1d8f
rodrigogomesrz <rodrigo.rozario.gomes@escola.pr.gov.br>
2025-06-23 09:03:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    criaCartao(
+        'Powerlifting', 
+        'Qual o principal objetivo de um treino de força bruta?', 
+        'Aumentar a força máxima em três movimentos fundamentais: agachamnento, supino e levantamento terra.'
+    );
+
+    criaCartao(
+        'Testosterona', 
+        'Qual a impôrtancia da testosterona no ganho de massa e performance?', 
+        'A testosterona é o principal hormônio responsável pelo aumento de massa muscular, força e recuperação muscular.'
+    );
+
+    criaCartao(
+        'Descanso', 
+        'Por que o descanso e o sono são tão importantes quanto o treino pesado?', 
+        'Porque é durante o sono profundo e o descanso que o corpo se recupera, reconstrói fibras musculares e libera hôrmonios anabólicos sem descanso não há crescimento.'
+    );
+
+    criaCartao(
+        'Disciplina',
+        'Qual é papel da disciplina no processo da transformação física?',
+        'A disciplina é o que separa os fracos dos comprometidos.'
+    );
+});
